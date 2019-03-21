1319b442eaf04d232996e45d8b1f8c3b8b0e2a60
DatoZuro <davidzuroshvili@192.168.0.101>
2019-03-21 21:43:04 +0400

    added map

--- js/main.js
+++ js/main.js
@@ -1,20 +1,13 @@
-function initMap() {
-
-    
-}
-
 function getLocation() {
     var x = document.getElementById("x");
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(initMap);
     } else {
       x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-function showPosition(position) {
-    var latlon = position.coords.latitude + "," + position.coords.longitude;
-    
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyAQWYSo3JfLK_MV8s3yaHO0osrv_lDbyck";
-    
-    document.getElementById("x").innerHTML = "<img src='"+img_url+"'>";
+function initMap(position) {
+    var latlon = {lat : position.coords.latitude , lng : position.coords.longitude};
+    var map = new google.maps.Map(document.getElementById("map"),{zoom : 4,center:latlon});
+
 }
