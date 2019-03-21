63130c79094f5f0a1240328679b51d596c8847de
DatoZuro <davidzuroshvili@192.168.0.101>
2019-03-21 21:51:08 +0400

    Minor Fix

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 function getLocation() {
     var x = document.getElementById("x");
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(initMap);
+      navigator.geolocation.getCurrentPosition(getPos);
     } else {
       x.innerHTML = "Geolocation is not supported by this browser.";
     }
 }
-function initMap(position) {
+var position;
+function getPos(pos) {
+    position = pos;
+}
+function initMap() {
     var latlon = {lat : position.coords.latitude , lng : position.coords.longitude};
     var map = new google.maps.Map(document.getElementById("map"),{zoom : 4,center:latlon});
 
