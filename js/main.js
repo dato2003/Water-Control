762455213f5e9342fd0e48277ef19f65bb1e307a
DatoZuro <davidzuroshvili@192.168.0.101>
2019-03-21 21:16:44 +0400

    Map Display

--- js/main.js
+++ js/main.js
@@ -12,7 +12,9 @@ function getLocation() {
     }
 }
 function showPosition(position) {
-    var x = document.getElementById("x");
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+    
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyAQWYSo3JfLK_MV8s3yaHO0osrv_lDbyck";
+    
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
 }
