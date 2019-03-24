1ae4a0111ae9ca794ee7230966a8fc37796373f1
DatoZuro <davidzuroshvili@192.168.0.101>
2019-03-24 23:54:28 +0400

    Minor Changes

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
-var latitude;
-var longitude;
-function getPos(pos) {
-    var coords = pos.coords;
-    latitude = coords.latitude;
-    longitude = coords.longitude;
-}
-function error(err) {
-    alert(err.code + ":" + err.message);
-}
-function getLocation() {
-    var x = document.getElementById("x");
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getPos,error);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
 function initMap() {
+    
+    var latitude;
+    var longitude;
+    
+    function getPos(pos) {
+        var coords = pos.coords;
+        latitude = coords.latitude;
+        longitude = coords.longitude;
+    }
+    
+    function error(err) {
+        alert(err.code + ":" + err.message);
+    }
+    
+    function getLocation() {
+        var x = document.getElementById("x");
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getPos,error);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    }
+
+
     var latlon = {lat: latitude , lng: longitude};
     var map = new google.maps.Map(document.getElementById("map"),{zoom : 14,center: latlon});
 }
