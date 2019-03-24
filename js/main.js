a32741530d3c0923e43a0a3888d76da76b6503ca
DatoZuro <davidzuroshvili@192.168.0.101>
2019-03-25 00:49:30 +0400

    Minor Changes

--- js/main.js
+++ js/main.js
@@ -6,33 +6,13 @@ function error(err) {
     alert(err.code + ":" + err.message);
 }
 
-async function getPos(pos) {
-    
-    /*var promise = new Promise((resolve,reject) => {
-        var coords = pos.coords;
-        latitude = coords.latitude;
-        longitude = coords.longitude;
-
-        if(latitude != null && longitude != null)
-        {
-            resolve({lat: latitude , lng: longitude});
-        }
-    });
-
-    latlon = await promise;
-    alert(latlon);*/
-    
+function getPos(pos) {
     var coords = pos.coords;
     latitude = coords.latitude;
     longitude = coords.longitude;
 
 
-    //alert(latitude + " " + longitude);
-    
-}
-
-function getLatLon() {
-    return {lat: latitude , lng: longitude};
+    alert(latitude + " " + longitude); 
 }
 
 function initMap() {
@@ -42,6 +22,8 @@ function initMap() {
     } else {
         alert("Geolocation is not supported by this browser.");
     }
-
-    var map = new google.maps.Map(document.getElementById("map"),{zoom : 14,center: getLatLon()});
+    setTimeout(function(){
+        var latlon = {lat: latitude, lng:longitude};
+        var map = new google.maps.Map(document.getElementById("map"),{zoom : 14, center: latlon});
+    },2000);
 }
